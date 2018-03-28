6e406c722104743216cb5f244c4dc6889c0e9306
koenigmm <marius.koenig@outlook.com>
2018-03-28 22:21:01 +0200

    Aufgabe 1.2 angelgt

--- Aufgabe 1.2/Program.cs
+++ Aufgabe 1.2/Program.cs
@@ -4,9 +4,12 @@ namespace Aufgabe_1._2
 {
     class Program
     {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
