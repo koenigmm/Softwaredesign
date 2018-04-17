84bfd8f55d427d99ce5e3d5cc4e6121e6a4692f1
koenigmm <marius.koenig@outlook.com>
2018-04-17 12:45:39 +0200

    Aufgabe3 angelgt

---
+++ Aufgabe_3_Zahlensysteme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aufgabe_3_Zahlensysteme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
