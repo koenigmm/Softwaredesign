5dc2c23c47743c43af1cdde1b11183c3d9eaaa62
koenigmm <marius.koenig@outlook.com>
2018-04-22 17:06:41 +0200

    Teil 1

--- Aufgabe_3_Zahlensysteme/Program.cs
+++ Aufgabe_3_Zahlensysteme/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Aufgabe_3_Zahlensysteme
 {
@@ -6,7 +7,54 @@ namespace Aufgabe_3_Zahlensysteme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ConvertDecimalToHexal(4));
+        }
+
+        public static int ConvertDecimalToHexal(int dec)
+        {
+            int hex = 6;
+            int arraySize = 4;
+            int[] hexaValue = new int[arraySize];
+            int i = 0;
+            int hexaOut = 0;
+            StringBuilder myStringbuilder = new StringBuilder();
+            //int reverseHexaValue = 0; 
+            
+            while (dec > 0)
+            {
+                hexaValue[i] = dec % hex;
+                i++;
+                dec = dec / hex;
+            }
+
+            Array.Reverse(hexaValue);
+
+            foreach (int k in hexaValue)
+            {
+                myStringbuilder.Append(k);
+            }
+
+            hexaOut = int.Parse(myStringbuilder.ToString());
+
+
+            return hexaOut;
+        }
+
+        public static int ConvertHexalToDezimal(int hexal )
+        {
+                        
+        }
+
+        public static int ReverseInt (int nonReverse)
+        {
+            int reverse = 0;
+            while (nonReverse >0)
+            {
+                int rest = nonReverse % 10;
+                reverse = (reverse * 10) + rest;
+                nonReverse /= 10;
+            }
+            return reverse;
         }
     }
 }
