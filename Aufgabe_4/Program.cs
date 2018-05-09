7255dce78b88db0a7f5ef48011f9207bbd3d7624
koenigmm <marius.koenig@outlook.com>
2018-05-09 12:25:50 +0200

    PrintSearch und bool gefunden

--- Aufgabe_4/Program.cs
+++ Aufgabe_4/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace Aufgabe_4
 {
-        class Program
+    class Program
     {
         static void Main(string[] args)
         {
             var tree = new Tree<String>();
             var root = tree.CreateNode("root");
             var child1 = tree.CreateNode("child1");
-            var child2 = tree.CreateNode("child1");
+            var child2 = tree.CreateNode("child2");
             var child3 = tree.CreateNode("Child3");
             var child4 = tree.CreateNode("Child4");
             var child5 = tree.CreateNode("Child5");
@@ -31,20 +31,21 @@ namespace Aufgabe_4
             child1.AppendChild(grand13);
             child1.AppendChild(grand14);
             child1.AppendChild(grand15);
+            child1.RemoveChild(grand14);
             var grand21 = tree.CreateNode("grand21");
             child2.AppendChild(grand21);
             child1.RemoveChild(grand12);
-            var searchList = root.FindNode("child1");
-            foreach(var c in searchList)
-            {
-                Console.WriteLine(c);
-            }
+            var searchList = root.FindNode("child2");
+            var serachList2 = root.FindNode("Child19");
+            root.PrintSearch(searchList);
+            root.PrintSearch(serachList2);
             root.PrintTree();
         }
     }
 
     public class Tree<T>
     {
+        private bool gefunden = false;
         public T Content;
         public List<Tree<T>> ListOfChildren = new List<Tree<T>>();
 
@@ -80,7 +81,21 @@ namespace Aufgabe_4
             return (ListOfChildren.FindAll(x => x.Content.Equals(searchNode)));
         }
 
-
+        public void PrintSearch(List<Tree<T>> input)
+        {
+            foreach (var c in input)
+            {
+                gefunden = true;
+                int k = 1;
+                Console.WriteLine("Gesuchte Node: " + c.Content + " Vorkommen: " + k);
+                k++;
+            }
+            if (gefunden == false)
+            {
+                Console.WriteLine ("Gesuchte Node nicht gefunden");
+            }
+            gefunden = false;
+        }
     }
 
 }
