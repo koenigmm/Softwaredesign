3657d6ea2a8bd12c594f7a48385dc4d23c90a60f
koenigmm <marius.koenig@outlook.com>
2018-05-30 12:29:40 +0200

    Konstruktor angelgt ID Private

--- Aufgabe_L07/DesPatternSingleton/Program.cs
+++ Aufgabe_L07/DesPatternSingleton/Program.cs
@@ -6,9 +6,16 @@ namespace DesPatternSingleton
 
     public class Person
     {
-        public string Name;
-        public int Age;
-        public int Id;
+        private string Name;
+        private int Age;
+        private int Id;
+
+        public Person(string _name, int _age)  //Konstruktor
+        {
+            Age = _age;
+            Name = _name;
+            Id = IDGenerator.GetInstance().GibMirNeId();
+        }
 
         public override string ToString()
         {
@@ -45,32 +52,30 @@ namespace DesPatternSingleton
         {
             return letzteID++;
         }
-    } 
+    }
 
     class Program
     {
 
         static void Main(string[] args)
         {
-            
+
 
             List<Person> personen = new List<Person>();
 
             // Eine Stelle, an der Personen angelegt werden
-            personen.Add(new Person{ Name="Dieter", Age=44,     Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Horst", Age=45,      Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Walter", Age=33,     Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Karl-Heinz", Age=22, Id = IDGenerator.GetInstance().GibMirNeId()});
+            personen.Add(new Person("Walter", 14));
+            personen.Add(new Person("Babara", 67));
+            personen.Add(new Person("Sieglinde", 47));
+            personen.Add(new Person("Brunhilde", 19));
 
 
             // Eine ANDERE Stelle, an der Personen angelegt werden
-            personen.Add(new Person{ Name="Brunhilde", Age=56,    Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Maria", Age=75,        Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Kunigunde", Age=22,    Id = IDGenerator.GetInstance().GibMirNeId()});
-            personen.Add(new Person{ Name="Tusnelda", Age=12,     Id = IDGenerator.GetInstance().GibMirNeId()});
-
-
-
+            personen.Add(new Person("Thomas", 12));
+            personen.Add(new Person("Gerhart", 24));
+            personen.Add(new Person("Waldtraut", 34));
+            personen.Add(new Person("Siegmund", 59));
+            personen.Add(new Person("Tuniglinde", 74));
 
 
             foreach (var person in personen)
