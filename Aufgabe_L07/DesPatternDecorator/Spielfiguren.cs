1616dd27e45addd270b12b5e2a7b4b7b7dea1158
koenigmm <marius.koenig@outlook.com>
2018-06-06 10:08:18 +0200

    Anpassung 3

--- Aufgabe_L07/DesPatternDecorator/Spielfiguren.cs
+++ Aufgabe_L07/DesPatternDecorator/Spielfiguren.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
+using System;
 namespace DesPatternDecorator
 {
     public class Spielfigur
@@ -57,29 +55,6 @@ namespace DesPatternDecorator
         {
             Console.Write("Räusper...");
             _original.Drohe();
-        }
-    }
-
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Spielfigur> figuren = new List<Spielfigur>();
-
-            figuren.Add(new Monster());
-            figuren.Add(new Held());
-            figuren.Add(new ErkaelteteFigur(new Held()));
-            figuren.Add(new ErkaelteteFigur(new ErkaelteteFigur(new Monster())));
-            figuren.Add(new HeisereFigur(new Monster()));
-            figuren.Add(new ErkaelteteFigur(new HeisereFigur(new Held())));
-
-            foreach(var spielfigur in figuren)
-            {
-                spielfigur.Drohe();
-                Console.WriteLine();
-            }
-
         }
     }
 }
