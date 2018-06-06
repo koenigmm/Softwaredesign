1989683eb6c05d9016c143ecbbe4c167e25643b4
koenigmm <marius.koenig@outlook.com>
2018-06-06 10:15:41 +0200

    Anpassung

--- Aufgabe_L07/DesPatternSingleton/Ausgabe.cs
+++ Aufgabe_L07/DesPatternSingleton/Ausgabe.cs
@@ -3,60 +3,7 @@ using System.Collections.Generic;
 
 namespace DesPatternSingleton
 {
-
-    public class Person
-    {
-        private string Name;
-        private int Age;
-        private int Id;
-        public static List<Person> globalPerson = new List<Person>();
-
-        public Person(string _name, int _age)  //Konstruktor
-        {
-            Age = _age;
-            Name = _name;
-            Id = IDGenerator.GetInstance().GibMirNeId();
-            globalPerson.Add(this);
-        }
-
-        public override string ToString()
-        {
-            return "Name:" + Name + ", Age: " + Age + ", " + "Id: " + Id;
-        }
-    }
-
-    /*
-    class GlobalVariables
-    {
-        // public static int letzteID = 1;
-        public static IDGenerator DerIdMacher = new IDGenerator();
-    }
-    */
-
-    public class IDGenerator
-    {
-        private IDGenerator()
-        {
-            letzteID = 1;
-        }
-
-        private static IDGenerator _instance;
-
-        public static IDGenerator GetInstance()
-        {
-            if (_instance == null)
-                _instance = new IDGenerator();
-            return _instance;
-        }
-
-        private int letzteID;
-        public int GibMirNeId()
-        {
-            return letzteID++;
-        }
-    }
-
-    class Program
+    class Ausgabe
     {
 
         static void Main(string[] args)
